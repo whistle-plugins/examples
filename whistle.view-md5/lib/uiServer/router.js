c2eedef4a7119b8920f1d0f1bef54b299e9b64d2
avwo <avwu@qq.com>
2021-12-24 20:26:49 +0800

    feat: generate md5

--- whistle.view-md5/lib/uiServer/router.js
+++ whistle.view-md5/lib/uiServer/router.js
@@ -1,13 +1,18 @@
-const crypto = require('crypto');
+const { createHash } = require('crypto');
+
+const toMD5 = (str) => {
+  const hash = createHash('sha256');
+  return hash.update(str, 'utf8').digest('hex');
+};
 
 module.exports = (router) => {
   router.get('/cgi-bin/md5', (ctx) => {
     const { text, base64 } = ctx.request.body;
     const result = { md5: '' };
     if (text && typeof text === 'string') {
-
+      result.md5 = toMD5(text);
     } else if (base64 && typeof base64 === 'string') {
-      
+      result.md5 = toMD5(Buffer.from(base64, 'base64'));
     }
     ctx.body = result;
   });
